ae07a90a0082f680d20d601fdc933dc276a1f230
Marcus Matthews <mlmatthews75@gmail.com>
2023-09-30 09:14:21 -0400

    added shapes.test.js file for jest

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 // dependencies
 const fs = require('fs');
 const inquirer = require('inquirer')
-const createLogo = require()
-
-function approveInput(input, message) {
-    if (input) {
-        return true;
-    } else {
-        console.log(message)
-        return false;
-    }
-}
+const {Shape, Square, Circle, Triangle} = require('./lib/shapes');
 
 // questions user answers
 const questions = [
     {
         type: 'input',
         name: 'text',
-        message: '1-3 Letter text inside shape:',
-        validate: input => approveInput(input, 'Add your 3 characters for inside your logo')
+        message: 'Enter up to 3 characters:',
     },
     {
         type: 'input',
         name: 'textColor',
-        message: 'Color your logo text:',
-        validate: input => approveInput(input, 'Choose the color of your text')
+        message: 'Enter a color for the text color:',
     },    
     {
         type: 'list',
         name: 'shape',
         message: 'Shape of your logo:',
         choices: ['Circle', 'Square', 'Triangle'],
-        default: ['None'],
+        default: ['Circle'],
     },
     {
         type: 'input',
         name: 'shapeColor',
-        message: 'Color of your shape:',
-        validate: input => approveInput(input, 'Choose the color of your logo shape')
+        message: 'Enter a color for the shape color:',
     },
 ];
 
@@ -47,12 +35,18 @@ function writeToFile(fileName, data) {
         })
 };
 
-function init() {
+function createLogo() {
     inquirer
         .prompt(questions)
         .then((data) => {
-            writeToFile('examples', data)
+            const svg = new logo();
+            svg.setText(data.logoText, data.logoTextColor)
+            const shape = eval(`new ${data.logoShape}()`)
+            shape.setColor(data.logoShapeColor)
+            svg.setShape(shape)
+            writeToFile('', svg.render())
         })
+        
 };
 
-init();
+createLogo();
